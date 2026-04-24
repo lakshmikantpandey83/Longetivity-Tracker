26161d9ed415c3a39941e28b79797d3e55811824
lakshmikantpandey83 <lakshmikant.pandey@gmail.com>
2026-04-24 14:42:08 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'longevity-tracker-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Sans:wght@300;400;500;600&display=swap'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching shell assets');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cacheRes => {
+      return cacheRes || fetch(event.request);
+    })
+  );
+});
